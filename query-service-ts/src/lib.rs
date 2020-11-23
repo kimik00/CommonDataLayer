02d3a3277bc77712fad539514fed0ef3a8123e89
Arkadiusz Zema <arkadiusz.zema@hitachi-powergrids.com>
2020-11-23 09:34:23 +0100

    remove unused schema imports

--- query-service-ts/src/lib.rs
+++ query-service-ts/src/lib.rs
@@ -1,8 +1,8 @@
-use schema::{query_client::QueryClient, ObjectIds, SchemaId};
+use schema::query_client::QueryClient;
+use tonic::transport::Channel;
 
 pub mod victoria;
 
 pub mod schema {
     tonic::include_proto!("query");
 }
-
