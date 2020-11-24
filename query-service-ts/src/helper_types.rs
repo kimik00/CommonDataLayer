3ee3bfa969e7dccee84b6443968c2d40e3ba5ff1
Arkadiusz Zema <arkadiusz.zema@hitachi-powergrids.com>
2020-11-24 16:45:57 +0100

    Add test

--- query-service-ts/src/helper_types.rs
+++ query-service-ts/src/helper_types.rs
@@ -10,3 +10,29 @@ pub struct DataPointDef {
 
 #[derive(Serialize)]
 pub struct DataPointSerializable(#[serde(with = "DataPointDef")] pub DataPoint);
+
+#[cfg(test)]
+mod tests {
+    use crate::helper_types::DataPointSerializable;
+    use crate::schema::DataPoint;
+    #[test]
+    fn it_works() {
+        let given_input = vec![
+            DataPointSerializable(DataPoint {
+                timestamp: "2017-01-15T01:30:15.01Z".to_string(),
+                value: 10.0,
+            }),
+            DataPointSerializable(DataPoint {
+                timestamp: "2017-01-15T01:30:17.01Z".to_string(),
+                value: 11.1,
+            }),
+        ];
+        let output = serde_json::to_string(&given_input);
+        // Fixme: Use more compact format of serializing
+        let expected_output ="[{\"timestamp\":\"2017-01-15T01:30:15.01Z\",\"value\":10.0},{\"timestamp\":\"2017-01-15T01:30:17.01Z\",\"value\":11.1}]".to_string();
+        match output {
+            Ok(a) => assert_eq!(a, expected_output),
+            Err(_) => assert!(false, "Serialization failed"),
+        }
+    }
+}
