103737f37856ec4390c3707aa5d237f7ba8f6a44
damszew <damian.szewczyk111@gmail.com>
2020-12-21 14:05:09 +0100

    Define test fixtures

--- tests/query_router/test_endpoints.py
+++ tests/query_router/test_endpoints.py
@@ -1,3 +1,4 @@
+import time
 import json
 import pytest
 import requests
@@ -55,6 +56,55 @@ def prepare(request, tmp_path):
                 yield qr, data, sid, expected
 
 
+@pytest.fixture
+def prepare_env(tmp_path):
+    with CdlEnv('.', postgres_config=PostgresConfig()) as env:
+        with QueryRouter('1024', '50103', 'http://localhost:50101') as qr:
+            with SchemaRegistry(str(tmp_path),
+                                "master",
+                                "localhost:9093",
+                                "schema_registry",
+                                "cdl.schema_registry.internal",
+                                "50101"
+                                ) as sr:
+
+                yield env, qr, sr
+
+
+@pytest.fixture
+def prepare_document_storage_env(prepare_env):
+    env, qr, sr = prepare_env
+    with QueryService('50102', PostgresConfig()) as _:
+        sid = sr.create_schema('test_schema',
+                               'cdl.document.input',
+                               'http://localhost:50102',
+                               '{}',
+                               0)
+
+        yield env, qr, sid
+
+
+@pytest.fixture
+def prepare_timeseries_env(prepare_env):
+    env, qr, sr = prepare_env
+    with QueryServiceTs('50104', VictoriaMetricsConfig()) as _:
+        sid = sr.create_schema('test_schema',
+                               'cdl.document.input',
+                               'http://localhost:50104',
+                               '{}',
+                               1)
+
+        yield qr, sid
+
+
+@pytest.fixture
+def prepare_data():
+    data, expected = load_case('query_ts', 'query_router')
+    insert_test_metrics(data['database_setup'])
+
+    yield data, expected
+
+
 def test_endpoint_multiple(prepare):
     qr, data, sid, expected = prepare
 
@@ -63,96 +113,48 @@ def test_endpoint_multiple(prepare):
     assert_json(response.json(), expected)
 
 
-def test_endpoint_single_ds(tmp_path):
-    with CdlEnv('.', postgres_config=PostgresConfig()) as env, QueryService('50102', PostgresConfig()) as _:
-        data, expected = load_case('query_ds', 'query_router')
+def test_endpoint_single_ds(prepare_document_storage_env):
+    env, qr, sid = prepare_document_storage_env
+    data, expected = load_case('query_ds', 'query_router')
 
-        with QueryRouter('1024', '50103', 'http://localhost:50101') as qr:
+    db = connect_to_postgres(env.postgres_config)
+    insert_test_data(db, data['database_setup'])
+    db.close()
 
-            db = connect_to_postgres(env.postgres_config)
-            insert_test_data(db, data['database_setup'])
-            db.close()
+    response = qr.query_get_single(sid, data['query_for'], "{}")
 
-            with SchemaRegistry(str(tmp_path),
-                                "master",
-                                "localhost:9093",
-                                "schema_registry",
-                                "cdl.schema_registry.internal",
-                                "50101"
-                                ) as sr:
-
-                sid = sr.create_schema('test_schema',
-                                       'cdl.document.input',
-                                       'http://localhost:50102',
-                                       '{}',
-                                       0)
-
-                response = qr.query_get_single(sid, data['query_for'], "{}")
-
-                assert_json(response.json(), expected)
+    assert_json(response.json(), expected)
 
 
-def test_endpoint_single_ts(tmp_path):
-    with CdlEnv('.') as env, QueryServiceTs('50104', VictoriaMetricsConfig()) as _:
-        data, expected = load_case('query_ts', 'query_router')
+def test_endpoint_single_ts(prepare_timeseries_env, prepare_data):
+    qr, sid = prepare_timeseries_env
+    data, expected = prepare_data
 
-        with QueryRouter('1024', '50103', 'http://localhost:50101') as qr:
+    time.sleep(1)
 
-            insert_test_metrics(data['database_setup'])
+    # Line protocol requires timestamps in [ns]
+    # Victoriametrics stores them internally in [ms]
+    # but PromQL queries use "unix timestamps" which are in [s]
+    start = 1608216910
+    end = 1608216919
+    step = 1
+    req_body = {"from": str(start), "to": str(
+        end), "step": str(step)}
 
-            with SchemaRegistry(str(tmp_path),
-                                "master",
-                                "localhost:9093",
-                                "schema_registry",
-                                "cdl.schema_registry.internal",
-                                "50101"
-                                ) as sr:
+    response = qr.query_get_single(
+        sid, data['query_for'], json.dumps(req_body))
 
-                sid = sr.create_schema('test_schema',
-                                       'cdl.document.input',
-                                       'http://localhost:50104',
-                                       '{}',
-                                       1)
-
-                # Line protocol requires timestamps in [ns]
-                # Victoriametrics stores them internally in [ms]
-                # but PromQL queries use "unix timestamps" which are in [s]
-                start = 1608216910
-                end = 1608216919
-                step = 1
-                req_body = {"from": str(start), "to": str(
-                    end), "step": str(step)}
-
-                response = qr.query_get_single(
-                    sid, data['query_for'], json.dumps(req_body))
-
-                assert_json(response.json(), expected)
+    assert_json(response.json(), expected)
 
 
-def test_endpoint_schema_ds(tmp_path):
-    with CdlEnv('.', postgres_config=PostgresConfig()) as env, QueryService('50102', PostgresConfig()) as _:
-        data, expected = load_case('query_ds_by_schema', 'query_router')
+def test_endpoint_schema_ds(prepare_document_storage_env):
+    env, qr, sid = prepare_document_storage_env
+    data, expected = load_case('query_ds_by_schema', 'query_router')
 
-        with QueryRouter('1024', '50103', 'http://localhost:50101') as qr:
+    db = connect_to_postgres(env.postgres_config)
+    insert_test_document(db, data, sid)
+    db.close()
 
-            with SchemaRegistry(str(tmp_path),
-                                "master",
-                                "localhost:9093",
-                                "schema_registry",
-                                "cdl.schema_registry.internal",
-                                "50101"
-                                ) as sr:
+    response = qr.query_get_schema(sid)
 
-                sid = sr.create_schema('test_schema',
-                                       'cdl.document.input',
-                                       'http://localhost:50102',
-                                       '{}',
-                                       0)
-
-                db = connect_to_postgres(env.postgres_config)
-                insert_test_document(db, data, sid)
-                db.close()
-
-                response = qr.query_get_schema(sid)
-
-                assert_json(response.json(), expected)
+    assert_json(response.json(), expected)
